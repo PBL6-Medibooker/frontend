ae36f467f09cc175f3aa9b65e7f53aff9d8bdef8
ThanhVViet <destielisdestiny1111@gmail.com>
2024-11-05 09:18:38 +0700

    5/11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar/Navbar";
+import {Route, Routes, useLocation} from "react-router-dom";
+import GolobalStyles from './globalStyles'
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Appointment from "./pages/Appointment/Appointment";
+import Doctors from "./pages/Doctor/Doctors";
+import DocInfo from "./pages/DocInfo/DocInfo";
+import Specialities from "./pages/Specialities/Specialities";
+// import Blog from "./pages/Blog";
+// import SpecialitiesInfo from "./pages/SpecialitiesInfo/SpecialitiesInfo";
+
 
 function App() {
+  const location = useLocation();
+
+  const isLRPage = location.pathname === '/login' || location.pathname === '/register' ;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        {!isLRPage && <Navbar/>}
+        <GolobalStyles/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/appointment' element={<Appointment/>}/>
+          <Route path='/doctors' element={<Doctors/>}/>
+          <Route path='/doctors/:speciality' element={<Doctors/>}/>
+          <Route path='/appointment/:docId' element={<DocInfo/>}/>
+          <Route path='/specialities' element={<Specialities/>}/>
+          <Route path='/specialities/:id' element={<SpecialitiesInfo/>}/>
+          <Route path='/blog' element={<Blog/>}/>
+
+
+        </Routes>
+        {!isLRPage && <Footer/>}
+      </div>
   );
 }
 
